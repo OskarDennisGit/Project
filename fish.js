9a1d7ffd82b3f65f335b882e8e6742407b46292c
Dines <dines.trier.hoj@gmail.com>
2026-05-06 13:30:25 +0200

    added seej food method

--- fish.js
+++ fish.js
@@ -20,15 +20,6 @@ class Fish {
     }
 
     draw() {
-        /*
-        fill("orange");
-        //push/pop bruges til at tegne hver fisk baseret på deres rotation i stedet for at rotere hele canvaset.
-        push();
-        translate(this.xpos, this.ypos);
-        rotate(this.direction);
-        triangle(0, 0, -this.size, -this.size/2, -this.size, this.size/2);
-        pop();*/
-       
     
     //tyvstjålet fra https://p5js.org/examples/classes-and-objects-flocking/ 
     //tegner trekanter baseret på deres position og retning, så de ser ud som om de svømmer i den retning de peger.
@@ -36,13 +27,13 @@ class Fish {
     fill("orange");
     stroke(255);
     push();
-    translate(this.position.x, this.position.y);
-    rotate(theta);
-    beginShape();
-    vertex(0, -this.size * 2);
-    vertex(-this.size, this.size * 2);
-    vertex(this.size, this.size * 2);
-    endShape(CLOSE);
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+            vertex(0, -this.size * 2);
+            vertex(-this.size, this.size * 2);
+            vertex(this.size, this.size * 2);
+        endShape(CLOSE);
     pop();
     }
 
@@ -190,6 +181,30 @@ class Fish {
             return true;
         } else {
             return false;
+        }
+    }
+
+    seekFood(foodArray) {
+        if (foodArray.length === 0) return;
+
+        let closest = null;
+        let closestDist = Infinity;
+
+        for (let i = 0; i < foodArray.length; i++) {
+            let d = p5.Vector.dist(this.position, foodArray[i].position);
+            if (d < closestDist) {
+                closestDist = d;
+                closest = i;
+            }
+        }
+
+        let steering = this.seek(foodArray[closest].position);
+        this.acceleration.add(steering);
+        
+        // Hvis fisken er tæt nok på maden, spis den og fjern den fra arrayet
+        if (closestDist < 4) {
+            foodArray.splice(closest, 1);
+            this.hunger++; // reset hunger after eating
         }
     }
 
