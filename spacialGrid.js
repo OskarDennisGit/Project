67a7fb5f50e6a5887c9b954af1342348460255ae
Dines <dines.trier.hoj@gmail.com>
2026-05-20 14:30:38 +0200

    minor change

--- spacialGrid.js
+++ spacialGrid.js
@@ -57,14 +57,14 @@ class SpacialGrid {
         let thisRow = Math.floor(fish.position.y / this.cellSize);
 
         // Tjek de omkringliggende celler (inklusiv den nuværende celle)
-        for (let cellx = -1; cellx <= 1; cellx++) {
-            for (let celly = -1; celly <= 1; celly++) {
-                // Beregner den nye kolonne og række med modulo for at håndtere wrap-around
-                let newCol = (thisCol + cellx + this.columns) % this.columns;
-                let newRow = (thisRow + celly + this.rows) % this.rows;
+        for (let dx = -1; dx <= 1; dx++) {
+            for (let dy = -1; dy <= 1; dy++) {
+                // Beregner den nye kolonne og række til at håndtere wrap-around
+                let newCol = (thisCol + dx + this.columns) % this.columns;
+                let newRow = (thisRow + dy + this.rows) % this.rows;
                 
                 // Tilføj alle fisk i den nye celle til naboerne
-                neighbors.push(...this.grid[newRow][newCol].boids);
+                neighbors.push(this.grid[newRow][newCol].boids);
             }
         }
         
